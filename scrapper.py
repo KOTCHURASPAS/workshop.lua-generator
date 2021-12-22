2ec0752a0f60a415253af618c754aa010267dbe4
no4ka <93768067+KOTCHURASPAS@users.noreply.github.com>
2021-12-23 00:45:47 +0300

    workshop.lua Generator

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,7 @@ import requests as r
 from bs4 import BeautifulSoup as BS
 
 surl = "https://steamcommunity.com/sharedfiles/filedetails/?id="
-print("Введите ID коллекции:")
+print("Enter collection ID:")
 wsc = input()
 
 for el in BS(r.get(surl + wsc).content, "html.parser").select(".collectionItem"):
