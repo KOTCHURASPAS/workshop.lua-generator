a12f981990df16685956c5fa8c5ab931769c70fe
no4ka <93768067+KOTCHURASPAS@users.noreply.github.com>
2022-01-14 20:55:02 +0300

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -2,8 +2,7 @@ import requests as r
 from bs4 import BeautifulSoup as BS
 
 surl = "https://steamcommunity.com/sharedfiles/filedetails/?id="
-print("Enter collection ID:")
-wsc = input()
+wsc = input("Enter collection ID:")
 
 for el in BS(r.get(surl + wsc).content, "html.parser").select(".collectionItem"):
     print("resource.AddWorkshop('" + el.find("a").attrs["href"].replace(surl, "") + "')" + " -- " + el.select(".collectionItemDetails a")[0].text + " -- by " + el.select(".collectionItemDetails .workshopItemAuthorName a")[0].text)
